d994aed0057d76a634d28dc22bff0c08f6043651
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2023-06-27 14:08:25 +0200

    test v 0.1

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-    return (
-        <main>
-            <h1>Hello World v2</h1>
-        </main>
-    );
+  return (
+    <main>
+      <h1>Hello World v2</h1>
+      <h1>Hello World v2</h1>
+      <h1>Hello World v2</h1>
+      <h1>Hello World v2</h1>
+      <h1>Hello World v2</h1>
+      <h1>Hello World v2</h1>
+    </main>
+  );
 }
