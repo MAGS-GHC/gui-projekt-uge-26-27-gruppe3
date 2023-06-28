c1c638cb130536b907379fed9a75ad3bc7e861df
Kim Mortensen <61045621+krixzy@users.noreply.github.com>
2023-06-28 13:59:35 +0200

    done for idag

--- app/firebaseConnect.js
+++ app/firebaseConnect.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp }  from "firebase/app";
-import 'firebase/database';
+import { initializeApp } from "firebase/app";
+import "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,14 +13,14 @@ const firebaseConfig = {
   storageBucket: "stadionbookingskoleproject.appspot.com",
   messagingSenderId: "400617141781",
   appId: "1:400617141781:web:d82f13bc82e58b640f801c",
-  measurementId: "G-XX8C3Y6QRP"
+  measurementId: "G-XX8C3Y6QRP",
 };
 const app = initializeApp(firebaseConfig);
 
 export default app;
 
-// The below code is what you need to get data from the database right now 
-// you get what documents that are under the collection "section" on the 
+// The below code is what you need to get data from the database right now
+// you get what documents that are under the collection "section" on the
 // database currently you only get "a" back
 // import firebase from './firebaseConnect.js';
 // import { getFirestore, collection, getDocs } from 'firebase/firestore';
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client";
-import H1 from "app/components/h1";
 import { useState } from "react";
-
 import KampPreview from "./components/KampPreview";
+
+//test
 
 export default function Home() {
   let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
