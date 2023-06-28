c8ae8abd4961ffee733485a10b75bbded668d2a5
Esben Christensen <80604101+esbenchristensen@users.noreply.github.com>
2023-06-28 10:58:31 +0200

    Landingpage

---
+++ app/components/KampPreview.jsx
@@ -0,0 +1,14 @@
+export default function KampPreview() {
+    return (
+        <div className="kampPreview border border-1">
+            <div className="p-5 border border-1 text-center font-semibold text-lx">
+                <h1>BFF - VFF</h1>
+            </div>
+            <div className="p-5">
+                <p>Dato</p>
+                <p>Pris</p>
+                <p>Antal ledige plasser</p>
+            </div>
+        </div>
+    );
+}
--- app/components/h1.jsx
+++
@@ -1,3 +0,0 @@
-export default function H1(props) {
-    return <h1>{props.tekst}</h1>;
-}
