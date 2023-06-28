cd3316d6c9f075e915947b6fbae4b99663276188
Esben Christensen <80604101+esbenchristensen@users.noreply.github.com>
2023-06-28 14:02:28 +0200

    Landingpage changes

--- app/components/WebHeader.jsx
+++ app/components/WebHeader.jsx
@@ -1,9 +1,7 @@
 export default function Header() {
     return (
         <div>
-            <div className="text-center p-10 header bg-dark-green">
-                <h1 className="font-semibold text-3xl">VFF Booking</h1>
-            </div>
+            <div className="text-center p-10 header bg-dark-green"></div>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -5,19 +5,22 @@ export default function Home() {
 
     return (
         <main>
-            <section className="titleSection">
-                <div className="container">
-                    <div className="text-center">
-                        <h1 className="font-semibold text-5xl">Stadion Booking</h1>
-                        <p className="mt-5 font-medium text-xl">
-                            Her finder du dine billetter til alle kampe på Viborg stadion
-                        </p>
-                    </div>
+            <section className="titleSection p-5">
+                <img
+                    className="w-1/4 mx-auto headerlogo"
+                    src="https://www.vff.dk/images/Logo/VFF_primaerlogo_cmyk_compr_med.png"
+                    alt="logo"
+                />
+                <div className="text-center p-52">
+                    <h1 className="font-semibold text-8xl text-white">Billet Booking</h1>
+                    <p className="mt-5 font-medium text-xl text-white">
+                        Her finder du billetter til alle kampe på Viborg stadion
+                    </p>
                 </div>
             </section>
             <section>
                 <div className="container">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                    <div className="grid grid-cols-1 gap-14">
                         {testArray.map((item, index) => {
                             return <KampPreview key={index} />;
                         })}
