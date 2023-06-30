864a10dbf5a329347e73bebe27615c4bf7eec15b
Erik Kjær Klint <erik0794@herningsholm.dk>
2023-06-30 09:54:28 +0200

    Did some stuff

---
+++ app/components/Message.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Message = ({ isOpen, message, onAccept }) => {
+    if (!isOpen) {
+        return null; // Render nothing if the modal is closed
+    }
+
+    return (
+        <div className="fixed inset-0 flex items-center justify-center z-10">
+            <div className="bg-dark-green w-64 p-4 rounded shadow">
+                <p className="text-center text-white">{message}</p>
+                <div className="flex justify-center mt-4">
+                    <button
+                        className="bg-green-500 text-white px-4 py-2 rounded"
+                        onClick={onAccept}
+                    >
+                        OK
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Message;
--- app/payment/page.js
+++ app/payment/page.js
@@ -1,4 +1,10 @@
+"use client";
+import { useState } from "react";
+import Message from '../components/Message';
+
 export default function Home() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <main>
       <h1 className="flex col-span-12 justify-center text-3xl mb-0 sm:mb-10 mt-4">
@@ -6,17 +12,25 @@ export default function Home() {
       </h1>
       <div className="flex justify-center h-screen sm:h-auto">
         <div className="flex flex-col sm:flex-row basis-1/3 sm:basis-2/3 justify-evenly text-4xl mt-0 sm:m-10">
-          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black">
-            Mobilpay
+          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black" onClick={() => setIsModalOpen(true)}>
+            MobilePay
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black">
+          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black" onClick={() => setIsModalOpen(true)}>
             Paypal
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black">
+          <button className="bg-blue-500 hover:bg-blue-700 hover:text-white font-bold py-2 px-10 border border-blue-700 rounded bg-grey hover:bg-black" onClick={() => setIsModalOpen(true)}>
             Card
           </button>
         </div>
       </div>
+    {isModalOpen && (
+        <Message
+            isOpen={isModalOpen}
+            message="Payment Successful"
+            onAccept={() => setIsModalOpen(false)}
+        />
+    )}
+
     </main>
   );
 }
